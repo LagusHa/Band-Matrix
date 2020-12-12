50fd4de6a6fcbae044d81ff7661e6d9f8eac0413
“Valery <glypw@mail.ru>
2020-12-13 01:22:36 +0300

    Вывод смежной матрицы по ребрам

--- public/index.php
+++ public/index.php
@@ -1,1 +1,30 @@
 <?php
+error_reporting(0);
+function debug($arr, $die = false){
+    echo '<pre>' . print_r($arr, true) . '</pre>';
+    if ($die) die();
+}
+$matrix = [
+    [1 => 2,1,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,6],
+    [1 => 4,4,4,5,7,8,9,10,11,12,13,14,15,16,17,18,19,8],
+];
+
+function createMatrix($matrix)
+{
+    for ($i = 1; $i < 20; $i++) {
+        for ($j = 1; $j < 20; $j++) {
+            $mass[$i][$j] = 0;
+            $mass[$matrix[0][$j]][$matrix[1][$j]] = 1;
+            $mass[$matrix[1][$j]][$matrix[0][$j]] = 1;
+        }
+    }
+    echo "<br>";
+    for ($i = 1; $i < 20; $i++) {
+        for ($j = 1; $j < 20; $j++) {
+            echo $mass[$i][$j] . " ";
+        }
+        echo "<br>";
+    }
+    debug($mass);
+}
+createMatrix($matrix);

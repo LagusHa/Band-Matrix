67d4533bb7c9b0b446ce87d5d132fd9a014f6195
“Valery <glypw@mail.ru>
2020-12-28 17:57:28 +0300

    Маленький рефакторинг :) Нужно нормально переписать код!

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ function debug($arr, $die = false){
 }
 ?>
 <form action="index.php" method="post">
-    <p><b>Выберите файл с матрицей</b></p>
+    <p><b>Напишите название файла с вершинами</b></p>
     <input type="text" name="name" value="19x19.txt">
     <input type="submit">
 </form>
@@ -17,74 +17,66 @@ function debug($arr, $die = false){
 if (!empty($_POST['name'])){
     $data = $_POST['name'];
     $matrix = getMatrix($data);
-    $mass = createMatrix($matrix);
-    $avC = getNeighbor($mass);
-    $output = createNewMatrix($mass);
+    $count = count(array_unique(array_merge($matrix[0],$matrix[1])));
+    $mass = createMatrix($matrix,$count);
+    $output = createNewMatrix($mass, $count);
     writeToTxt($output);
 }
-//$matrix = [
-////    [1 => 2,1,3,5,5,5,5,5,5,5,5,5,5,5,5,5,5,7],
-////    [1 => 5,5,5,4,8,6,9,10,11,12,13,14,15,16,17,18,19,6],
-//    [1 => 2,1,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,6],
-//    [1 => 4,4,4,5,7,8,9,10,11,12,13,14,15,16,17,18,19,8],
-//];
-//
 
-
-function createMatrix($matrix)
+function createMatrix($matrix, $count)
 {
-    for ($i = 1; $i < 20; $i++) {
-        for ($j = 1; $j < 20; $j++) {
+    for ($i = 1; $i < $count + 1; $i++) {
+        for ($j = 1; $j < $count + 1; $j++) {
             $mass[$i][$j] = 0;
             $mass[$matrix[0][$j]][$matrix[1][$j]] = 1;
             $mass[$matrix[1][$j]][$matrix[0][$j]] = 1;
         }
     }
-    showMatrix($mass);
-    echo getNeighbor($mass) . "<br>";
+    showMatrix($mass, $count);
+    echo getNeighbor($mass, $count) . "<br>";
     return $mass;
 }
 
-function showMatrix($matrix){
-    for ($i = 1; $i < 20; $i++) {
-        for ($j = 1; $j < 20; $j++) {
+function showMatrix($matrix, $count){
+    for ($i = 1; $i < $count + 1; $i++) {
+        for ($j = 1; $j < $count + 1; $j++) {
             echo $matrix[$i][$j] . " ";
         }
         echo "<br>";
     }
 }
 
-function getNeighbor($matrix){
-    for ($i = 1; $i < 20; $i++){
+function getNeighbor($matrix, $count){
+    for ($i = 1; $i < $count + 1; $i++){
         foreach ($matrix[$i] as $k => $v){
             if ($v == 0){
                 unset($matrix[$i][$k]);
             }
         }
     }
-    $avC = getAverageCount($matrix);
+    $avC = getAverageCount($matrix, $count);
     return $avC;
 }
 
-function getAverageCount($matrix){
-    for ($i = 1; $i < 20; $i++) {
+function getAverageCount($matrix, $count){
+    for ($i = 1; $i < $count + 1; $i++) {
         foreach ($matrix[$i] as $k => $v) {
             $absCount[$i][$k] = abs($i - $k);
         }
     }
-    for ($i = 1; $i < 20; $i++) {
+    for ($i = 1; $i < $count + 1; $i++) {
         $avCount[$i] = max($absCount[$i]);
     }
     $averageCount = round(array_sum($avCount)/count($avCount), 5);
     return $averageCount;
 }
 
-function switchRowsAndColumns($mass, $a, $b)
+function switchRowsAndColumns($mass, $a, $b, $count)
 {
     $tmp = $mass[$a];
     $mass[$a] = $mass[$b];
     $mass[$b] = $tmp;
-    for ($i = 1; $i < 20; $i++) {
+    for ($i = 1; $i < $count + 1; $i++) {
         $tmp = $mass[$i][$a];
         $mass[$i][$a] = $mass[$i][$b];
         $mass[$i][$b] = $tmp;
@@ -92,10 +84,10 @@ function switchRowsAndColumns($mass, $a, $b)
     return $mass;
 }
 //Можно сделать лучше (отрефакторить)
-function createNewMatrix($matrix){
+function createNewMatrix($matrix, $count){
     $i = 0;
     $mass = $matrix;
-    $av = getNeighbor($matrix);
+    $av = getNeighbor($matrix, $count);
     foreach ($mass as $k => $v){
        $stack[$i][$k] = $k;
     }
@@ -108,8 +100,8 @@ function createNewMatrix($matrix){
     $empiricalСycle = round(3 + count($stack[1])/100, 0);
     while ($i < $empiricalСycle) {
         while ($b < count($stack[$i])) {
-            $mass = switchRowsAndColumns($mass, $a, $b);
-            $lengt = getNeighbor($mass);
+            $mass = switchRowsAndColumns($mass, $a, $b, $count);
+            $lengt = getNeighbor($mass, $count);
             if ($av > $lengt) {
                 $av = $lengt;
                 $matrix = $mass;
@@ -135,7 +127,7 @@ function createNewMatrix($matrix){
     $i++;
     }
     echo "<br>";
-    showMatrix($mass);
+    showMatrix($mass, $count);
     echo $lengt;
     return $mass;
 }
